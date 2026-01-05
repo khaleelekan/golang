0d843a724292a85ac4f66b21a538fdd327729bdd
khaleelekan <kalilulekan@gmail.com>
2026-01-05 12:36:40 +0000

    learning go lang

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+
+import "fmt"
+
+func main() {
+	var greeting string = "Hello, Go backend world!"
+	var name = "alkhaleely"
+	var age int = 30
+
+	fmt.Println("greeting:", name, greeting, "you are" ,age ,"years old")
+}
+
+func stringTypes() {
+	var str1 string = "Hello, World!"
+}
