c20d4a2649ad3d8eeb005a221d3317305dd9f497
khaleelekan <kalilulekan@gmail.com>
2026-01-05 14:40:28 +0000

    o values

--- main.go
+++ main.go
@@ -11,6 +11,21 @@ func main() {
 	fmt.Println("greeting:", name, greeting, "you are" ,age ,"years old")
 }
 
-func stringTypes() {
-	var str1 string = "Hello, World!"
+// func stringTypes() {
+// 	var str1 string = "Hello, World!"
+// }
+
+func zeroValues() {
+    var a int     // 0
+    var b float64 // 0.0
+    var c string  // "" (empty string)
+    var d bool    // false
+    // var e *int    // nil (pointer)
+    // var f []int   // nil (slice)
+    // var g map[string]int // nil (map)
+    
+    fmt.Println("int zero:", a)
+    fmt.Println("float zero:", b)
+    fmt.Println("string zero:", c) // Will print nothing
+    fmt.Println("bool zero:", d)
 }
